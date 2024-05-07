6e79415d81740adfe8f320f685fc8ccb1f99ad8c
YuZeXingHe <112630008+YuZeXingHe@users.noreply.github.com>
2024-05-07 21:04:08 +0800

    Add files via upload

---
+++ src/main/java/com/yuzexingheplugin/Command/config.java
@@ -0,0 +1,194 @@
+package com.yuzexingheplugin.Command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class config implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "该指令只能由玩家执行！");
+            return false;
+        }
+
+        Player player = (Player) sender;
+        Plugin plugin = com.yuzexingheplugin.YuZeXingHePlugin.getProvidingPlugin(com.yuzexingheplugin.YuZeXingHePlugin.class);
+        String hand = "[YuZeXingHePlugin]";
+
+        // UI界面配置
+        Boolean All_UI = plugin.getConfig().getBoolean("all_ui");
+
+        // 世界监听配置
+        Boolean Server_Listener = plugin.getConfig().getBoolean("server_listener");
+        Boolean Player_Join = plugin.getConfig().getBoolean("player_join");
+        Boolean Player_Quit = plugin.getConfig().getBoolean("player_quit");
+        Boolean Player_Death = plugin.getConfig().getBoolean("player_death");
+        Boolean Player_Deep_Sleep = plugin.getConfig().getBoolean("player_deep_sleep");
+
+        // 玩家活动监听配置
+        Boolean Player_Listenter = plugin.getConfig().getBoolean("player_listenter");
+
+        if (args.length < 1) {
+            player.sendMessage(ChatColor.RED + "请输入正确的参数！");
+            return false;
+        }
+
+        if (!player.isOp()) {
+            player.sendMessage(ChatColor.RED + "你没有权限执行此命令！");
+            return false;
+        }
+
+        String config_command = args[0];
+        if (config_command.equals("start")) {
+            player.sendMessage(ChatColor.YELLOW + hand + ChatColor.GREEN + plugin.getConfig().getString("start"));
+        }
+
+        else if (config_command.equals("list")) {
+            // UI界面开关列表
+            sender.sendMessage(ChatColor.YELLOW + hand + ChatColor.GREEN + "UI界面开关列表：");
+            // UI界面功能总开关
+            if (All_UI) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("all_ui_true"));
+            }
+            if (!All_UI) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("all_ui_false"));
+            }
+            // 服务器监听总开关
+            sender.sendMessage(ChatColor.YELLOW + hand + ChatColor.GREEN + "服务器监听开关列表：");
+            if (Server_Listener) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("server_listener_true"));
+            }
+            if (!Server_Listener) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("server_listener_false"));
+            }
+            // 服务器监听玩家加入游戏开关
+            if (Player_Join) {
+                sender.sendMessage(ChatColor.DARK_AQUA + plugin.getConfig().getString("player_join_true"));
+            }
+            if (!Player_Join) {
+                sender.sendMessage(ChatColor.DARK_AQUA + plugin.getConfig().getString("player_join_false"));
+            }
+            // 服务器监听玩家离开游戏开关
+            if (Player_Quit) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("player_quit_true"));
+            }
+            if (!Player_Quit) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("player_quit_false"));
+            }
+            // 服务器监听玩家死亡事件开关
+            if (Player_Death) {
+                sender.sendMessage(ChatColor.DARK_AQUA + plugin.getConfig().getString("player_death_true"));
+            }
+            if (!Player_Death) {
+                sender.sendMessage(ChatColor.DARK_AQUA + plugin.getConfig().getString("player_death_false"));
+            }
+            // 服务器监听玩家深度睡眠事件开关
+            if (Player_Deep_Sleep) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("player_deep_sleep_true"));
+            }
+            if (!Player_Deep_Sleep) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("player_deep_sleep_false"));
+            }
+            // 服务器监听玩家活动内容总开关
+            sender.sendMessage(ChatColor.YELLOW + hand + ChatColor.GREEN + "玩家行为活动监听列表：");
+            if (Player_Listenter) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("player_listenter_true"));
+            }
+            if (!Player_Listenter) {
+                sender.sendMessage(ChatColor.AQUA + plugin.getConfig().getString("player_listenter_false"));
+            }
+        }
+        else if (config_command.equals("change")) {
+            if (args.length < 3) {
+                player.sendMessage(ChatColor.RED + "请输入完整的修改指令！");
+                return false;
+            }
+
+            String config_changes = args[1];
+            // 加载插件参数修改
+            if (config_changes.equals("start") || config_changes.equals("start_def")) {
+                plugin.getConfig().set(config_changes, args[2]);
+            }
+
+            // 快捷UI界面参数修改
+            else if (config_changes.equals("all_ui")) {
+                if (args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false")) {
+                    plugin.getConfig().set(config_changes, Boolean.valueOf(args[2]));
+                }
+                else {
+                    player.sendMessage(ChatColor.RED + "参数只能为 true 或 false！");
+                    return false;
+                }
+            }
+
+            // 服务器监听参数修改
+            else if (config_changes.equals("server_listener") || config_changes.equals("player_join") || config_changes.equals("player_quit") || config_changes.equals("player_death") || config_changes.equals("player_deep_sleep")) {
+                if (args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false")) {
+                    plugin.getConfig().set(config_changes, Boolean.valueOf(args[2]));
+                }
+                else {
+                    player.sendMessage(ChatColor.RED + "参数只能为 true 或 false！");
+                    return false;
+                }
+            }
+
+            // 服务器玩家活动监听参数修改
+            else if (config_changes.equals("player_listenter")) {
+                plugin.getConfig().set(config_changes, Boolean.valueOf(args[2]));
+            }
+
+            // 当找不到config配置时发送
+            else {
+                player.sendMessage(ChatColor.RED + "未知的配置项！");
+                return false;
+            }
+
+            plugin.saveConfig();
+            plugin.reloadConfig();
+            player.sendMessage(ChatColor.GREEN + "修改配置成功！");
+        }
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            completions.add("start");
+            completions.add("list");
+            completions.add("change");
+        }
+
+        else if (args.length == 2 && args[0].equals("change")) {
+            completions.add("start");
+            completions.add("start_def");
+            completions.add("all_ui");
+            completions.add("server_listener");
+            completions.add("player_join");
+            completions.add("player_quit");
+            completions.add("player_death");
+            completions.add("player_deep_sleep");
+            completions.add("player_listenter");
+        }
+
+        else if (args.length == 3 && (args[1].equals("server_listener") || args[1].equals("player_join") || args[1].equals("player_quit") || args[1].equals("player_death") || args[1].equals("player_deep_sleep"))) {
+            completions.add("true");
+            completions.add("false");
+        }
+
+        else if (args.length == 3 && (args[1].equals("player_listenter"))) {
+            completions.add("true");
+            completions.add("false");
+        }
+
+        return completions;
+    }
+}
---
+++ src/main/java/com/yuzexingheplugin/Listener/SeverListener.java
@@ -0,0 +1,206 @@
+package com.yuzexingheplugin.Listener;
+
+import io.papermc.paper.event.player.PlayerDeepSleepEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+
+public class SeverListener implements Listener {
+
+    public static Plugin pl = com.yuzexingheplugin.YuZeXingHePlugin.getProvidingPlugin(com.yuzexingheplugin.YuZeXingHePlugin.class);
+    String hand = "[YuZeXingHePlugin]";
+
+    // UI界面监听开关
+    public static boolean All_UI() {
+        return pl.getConfig().getBoolean("all_ui");
+    }
+    // 服务器监听开关
+    public static boolean Server_Listener() {
+        return pl.getConfig().getBoolean("server_listener");
+    }
+    // 服务器玩家加入事件监听开关
+    public static boolean Player_Join_Game() {
+        return pl.getConfig().getBoolean("player_join");
+    }
+    // 服务器玩家离开事件开关
+    public static boolean Player_Quit_Game() {
+        return pl.getConfig().getBoolean("player_quit");
+    }
+    // 服务器玩家死亡事件开关
+    public static boolean Players_Death() {
+        return pl.getConfig().getBoolean("player_death");
+    }
+    // 服务器玩家进入深度睡眠事件
+    public static boolean Players_Deep_Sleep() {
+        return pl.getConfig().getBoolean("player_deep_sleep");
+    }
+
+    // UI界面监听（玩家使用UI界面时触发）
+    @EventHandler
+    public void check(InventoryClickEvent left_click) {
+        Player player = (Player) left_click.getWhoClicked();
+            if (Server_Listener()) {
+                if (All_UI()) {
+                if (left_click.getWhoClicked().getOpenInventory().getTitle().equals("YuZeXingHe快捷UI：第1页")) {
+                    left_click.setCancelled(true);
+                    // UI界面：将玩家的游戏切换为创造模式（管理员）
+                    if (left_click.getRawSlot() == 0) {
+                        if (player.isOp()) {
+                            player.setGameMode(GameMode.CREATIVE);
+                            player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                            player.sendMessage(ChatColor.YELLOW + "成功将" + player.getName() + "的游戏模式设置为创造模式！");
+                        } else {
+                            player.sendMessage(ChatColor.RED + "你无权使用这个命令！");
+                        }
+                    }
+
+                    // UI界面：将玩家的游戏模式设置为生存模式（管理员）
+                    else if (left_click.getRawSlot() == 1) {
+                        if (player.isOp()) {
+                            player.setGameMode(GameMode.SURVIVAL);
+                            player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                            player.sendMessage(ChatColor.YELLOW + "成功将" + player.getName() + "的游戏模式设置为生存模式！");
+                        } else {
+                            player.sendMessage(ChatColor.RED + "你无权使用这个命令！");
+                        }
+                    }
+
+                    // UI界面：将玩家的游戏模式设置为冒险模式（管理员）
+                    else if (left_click.getRawSlot() == 2) {
+                        if (player.isOp()) {
+                            player.setGameMode(GameMode.ADVENTURE);
+                            player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                            player.sendMessage(ChatColor.YELLOW + "成功将" + player.getName() + "的游戏模式设置为冒险模式！");
+                        } else {
+                            player.sendMessage(ChatColor.RED + "你无权使用这个命令！");
+                        }
+                    }
+
+                    // UI界面：将玩家的游戏模式设置为旁观者模式（管理员）
+                    else if (left_click.getRawSlot() == 3) {
+                        if (player.isOp()) {
+                            player.setGameMode(GameMode.SPECTATOR);
+                            player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                            player.sendMessage(ChatColor.YELLOW + "成功将" + player.getName() + "的游戏模式设置为旁观者模式！");
+                        } else {
+                            player.sendMessage(ChatColor.RED + "你无权使用这个命令！");
+                        }
+                    }
+
+                    // UI界面：关闭服务器功能（管理员）
+                    else if (left_click.getRawSlot() == 4) {
+                        if (player.isOp()) {
+                            player.sendMessage(ChatColor.YELLOW + "所选功能执行成功，即将关闭服务器");
+                            Bukkit.getServer().shutdown();
+                        } else {
+                            player.sendMessage(ChatColor.RED + "你无权使用这个命令！");
+                        }
+                    }
+
+                    // UI界面：随身织布机功能（所有玩家）
+                    else if (left_click.getRawSlot() == 5) {
+                        player.openLoom(null, true);
+                        player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                    }
+
+                    // UI界面：随身铁砧功能（所有玩家）
+                    else if (left_click.getRawSlot() == 6) {
+                        player.openAnvil(null, true);
+                        player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                    }
+
+                    // UI界面：随身工作台功能（所有玩家）
+                    else if (left_click.getRawSlot() == 7) {
+                        player.openWorkbench(null, true);
+                        player.sendMessage(ChatColor.GREEN + "所选功能执行成功！");
+                    }
+
+                    // UI界面：玩家自杀功能（所有玩家）
+                    else if (left_click.getRawSlot() == 8) {
+                        player.sendMessage(ChatColor.GREEN + "自杀成功！");
+                        player.setHealth(0);
+                    }
+
+                    // UI界面：清空自己的背包（所有玩家）
+                    else if (left_click.getRawSlot() == 53) {
+                        player.getInventory().clear();
+                        player.saveData();
+                        player.sendMessage(ChatColor.RED + "成功清空你的背包！");
+                    }
+                }
+                if (left_click.getRawSlot() > left_click.getInventory().getSize()) {
+                    return;
+                }
+                if (left_click.getRawSlot() < 0) {
+                    return;
+                }
+            }
+        }
+        else {
+            player.sendMessage(ChatColor.RED + "服务器尚未启用该命令！");
+        }
+    }
+
+    // 玩家加入服务器事件
+    @EventHandler
+    public void Player_Join(PlayerJoinEvent event) {
+            if (Server_Listener()) {
+                if (Player_Join_Game()) {
+                Player player = event.getPlayer();
+                String player_name = player.getName();
+                event.setJoinMessage(ChatColor.YELLOW + "加入服务器：欢迎" + ChatColor.AQUA + player_name + ChatColor.YELLOW + "加入服务器！");
+                player.sendMessage(ChatColor.YELLOW + "使用/ui help来查看必要指令帮助！");
+            }
+        }
+    }
+
+    // 玩家离开服务器事件
+    @EventHandler
+    public void Player_Quit(PlayerQuitEvent event) {
+            if (Server_Listener()) {
+                if (Player_Quit_Game()) {
+                Player player = event.getPlayer();
+                String player_name = player.getName();
+                event.setQuitMessage(ChatColor.YELLOW + "离开服务器：" + ChatColor.AQUA + player_name + ChatColor.YELLOW + "离开了服务器！");
+            }
+        }
+    }
+
+    // 玩家死亡事件
+    @EventHandler
+    public void Player_Death(PlayerDeathEvent event) {
+        if (Server_Listener()) {
+            if (Players_Death()) {
+                Player player = event.getPlayer();
+                String player_name = player.getName();
+                int deathCount = player.getStatistic(org.bukkit.Statistic.DEATHS);
+                Location playerLocation = player.getLocation();
+                if (deathCount <= 1) {
+                    event.setDeathMessage(ChatColor.YELLOW + "死亡消息：" + player_name + "给自己玩没了，服务器直接笑TA！当前死亡次数：" + deathCount);
+                } else {
+                    event.setDeathMessage(ChatColor.YELLOW + "死亡消息：" + player_name + "又给自己玩没了，服务器已经笑岔气了！当前死亡次数：" + deathCount);
+                }
+            }
+        }
+    }
+
+    // 玩家进入深度睡眠事件
+    @EventHandler
+    public void Player_DeepSleep(PlayerDeepSleepEvent event) {
+        if (Server_Listener()) {
+            if (Players_Deep_Sleep()) {
+                Player player = event.getPlayer();
+                player.sendMessage(ChatColor.YELLOW + "睡一觉，明天就没有幻翼啦，晚安！");
+            }
+        }
+    }
+}
