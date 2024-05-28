98aedaa6340546540f1abab94cd45861939cb1e0
YuZeXingHe <112630008+YuZeXingHe@users.noreply.github.com>
2024-05-28 09:08:26 +0800

    Add files via upload

--- YuZeXingHePlugin/src/main/java/com/yuzexingheplugin/PlayerLevel/LevelConfig.java
+++ YuZeXingHePlugin/src/main/java/com/yuzexingheplugin/PlayerLevel/LevelConfig.java
@@ -26,42 +26,14 @@ public class LevelConfig implements CommandExecutor, TabExecutor {
             Player player = (Player) sender;
             // 判断玩家是否为服务器管理员
             if (player.isOp()) {
-                if (args.length > 1) {
+                if (args.length > 0) {
                     String Command1 = args[0];
-                    String Command2 = args[1];
                     // 修改配置文件相关内容
-                    if (Command1.equals("blockbreak") || Command1.equals("killedCreatures") || Command1.equals("LevelUp") || Command1.equals("MaxLevel")) {
+                    if (args.length > 1 && Command1.equals("blockbreak") || Command1.equals("killedCreatures") || Command1.equals("LevelUp") || Command1.equals("MaxLevel") || Command1.equals("blockbreakExperience") || Command1.equals("killedCreaturesExperience")) {
+                        String Command2 = args[1];
                         plugin.getConfig().set(Command1, Integer.valueOf(Command2));
                         player.sendMessage(ChatColor.GREEN + "修改配置文件成功！");
                     }
-                }
-                else {
-                    player.sendMessage(ChatColor.RED + "请输入正确的参数！");
-                }
-            }
-        }
-        else {
-            if (args.length > 1) {
-                String Command1 = args[0];
-                String Command2 = args[1];
-                // 修改配置文件相关内容
-                if (Command1.equals("blockbreak") || Command1.equals("killedCreatures") || Command1.equals("LevelUp") || Command1.equals("MaxLevel") || Command1.equals("blockbreakExperience") || Command1.equals("killedCreaturesExperience")) {
-                    plugin.getConfig().set(Command1, Integer.valueOf(Command2));
-                    Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "修改配置文件成功！");
-                }
-            }
-            else {
-                Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "请输入正确的参数！");
-            }
-        }
-
-        // 等级设定帮助
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            if (player.isOp()) {
-                if (args.length > 0) {
-                    String Command1 = args[0];
-                    // 玩家帮助列表
                     if (Command1.equals("help")) {
                         player.sendMessage(ChatColor.GREEN + "等级功能配置帮助列表");
                         player.sendMessage(ChatColor.AQUA + "/levelconfig blockbreak <数字数值>：修改玩家需要挖掘多少方块才能获得经验。");
@@ -80,6 +52,12 @@ public class LevelConfig implements CommandExecutor, TabExecutor {
         else {
             if (args.length > 0) {
                 String Command1 = args[0];
+                // 修改配置文件相关内容
+                if (args.length > 1 && Command1.equals("blockbreak") || Command1.equals("killedCreatures") || Command1.equals("LevelUp") || Command1.equals("MaxLevel") || Command1.equals("blockbreakExperience") || Command1.equals("killedCreaturesExperience")) {
+                    String Command2 = args[1];
+                    plugin.getConfig().set(Command1, Integer.valueOf(Command2));
+                    Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "修改配置文件成功！");
+                }
                 if (Command1.equals("help")) {
                     Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "等级功能配置帮助列表");
                     Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "/levelconfig blockbreak <数字数值>：修改玩家需要挖掘多少方块才能获得经验。");
